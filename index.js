1ead244eb92d10aae75806827663ed49677b63a0
lekekbot <alim97460@gmail.com>
2021-12-18 16:39:03 +0800

    rules and about (logo not centered)

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const canvas = document.getElementById('Matrix');
 const context = canvas.getContext('2d');
 
+window.addEventListener('resize', () => {
+	canvas.width = window.innerWidth
+	canvas.height = window.innerHeight
+})
+
+window.addEventListener('scroll', e => {
+	let arrow = document.getElementById('center-arrow')
+	arrow.style.opacity = 1 - (window.scrollY / 200)
+})
+
+/**
+ * const checkpoint = 300;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= checkpoint) {
+    opacity = 1 - currentScroll / checkpoint;
+  } else {
+    opacity = 0;
+  }
+  document.querySelector(".front").style.opacity = opacity;
+});
+ */
+
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -22,12 +47,12 @@ const draw = () => {
 	context.fillStyle = '#0F0';
 	context.font = fontSize + 'px monospace';
 
-	for(let i = 0; i < rainDrops.length; i++)
+	for(i = 0; i < rainDrops.length; i++)
 	{
 		const text = nums.charAt(Math.floor(Math.random() * nums.length));
 		context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
 		
-		if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
+		if(rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975){
 			rainDrops[i] = 0;
         }
 		rainDrops[i]++;
@@ -35,3 +60,5 @@ const draw = () => {
 };
 
 setInterval(draw, 30);
+
+
